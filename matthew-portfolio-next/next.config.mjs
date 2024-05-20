55a62889c4c65ca34a298e19677966e0b08b6e17
Matthew Coleman <matthewcoleman0066@gmail.com>
2024-05-20 08:20:24 -0400

    Added a workflow, adjusted readme

--- matthew-portfolio-next/next.config.mjs
+++ matthew-portfolio-next/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/Coleman-Matthew-Portfolio",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 export default nextConfig;
