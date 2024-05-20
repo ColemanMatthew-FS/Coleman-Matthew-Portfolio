bdc5e3965a120b4ff2572bdad09a55f5b34fdced
Matthew Coleman <matthewcoleman0066@gmail.com>
2024-05-20 11:11:19 -0400

    Added a simple redirect page to account for the change in my URL structure

--- matthew-portfolio-next/next.config.mjs
+++ matthew-portfolio-next/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     basePath: "/Coleman-Matthew-Portfolio",
     output: "export",  // <=== enables static exports
+    images: { unoptimized: true }, // <=== output export disabled image optimization, so I have to set this
     reactStrictMode: true,
 };
 
