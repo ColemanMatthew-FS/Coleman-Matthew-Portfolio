cc3744f7ffa981b1ee260faabb186a49ca1b55c2
Matthew Coleman <matthewcoleman0066@gmail.com>
2024-06-10 08:19:33 -0400

    feat: Added thumbnails to the project pages when viewed through mobile sized viewports

--- matthew-portfolio-next/components/Project.js
+++ matthew-portfolio-next/components/Project.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function Project({image, title, text, href}) {
     return(
-        <a href={href} className="w-full h-full md:w-[50vh] grow rounded-xl">
-            <h2 className="pt-3 pb-3 text-2xl md:font-semibold">{title}</h2>
+        <a href={href} className="flex flex-row-reverse justify-end items-center gap-x-6 md:block w-full h-full md:w-[50vh] grow">
+            <h2 className="pt-3 pb-3 md:text-2xl md:font-semibold">{title}</h2>
             <div className={styles.imageHolder}>
                 <Image
                 id={image.id}
@@ -14,7 +14,7 @@ export default function Project({image, title, text, href}) {
                 height={image.height}
                 alt={image.alt}
                 {...image}
-                className="hidden md:block object-cover overflow-hidden rounded-xl"
+                className="md:block object-cover h-24 w-24 md:h-full md:w-full overflow-hidden rounded-r-xl md:rounded-xl max-w-fit"
                 />
                 <p className="p-3 hidden md:block">{text}</p>
             </div>
